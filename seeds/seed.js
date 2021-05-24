275d05e4de3b18c1710bbaeb3422f37c08e43b7e
Erich Purpur <epurpur@gmail.com>
2021-05-24 16:54:11 -0400

    finalizing seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
 
 const seedUsers = require('./user-seeds');
-const seedBlogPosts = require('.blogpost-seeds');
-const seedComments = require('.comment-seeds');
+const seedBlogPosts = require('./blogpost-seeds');
+const seedComments = require('./comment-seeds');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
