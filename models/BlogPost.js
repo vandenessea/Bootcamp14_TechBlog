47af184c4b854d01187663a3696905ca2e597b56
Erich Purpur <epurpur@gmail.com>
2021-05-24 12:47:48 -0400

    building models

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -6,17 +6,30 @@ class BlogPost extends Model {}
 BlogPost.init(
     {
         id: {
-
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
         title: {
-            
+            type: DataTypes.STRING,
+            allowNull: false,
         },
         content: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
         },
         date: {
-
-        }
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        userID: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
     },
     {
         sequelize,
