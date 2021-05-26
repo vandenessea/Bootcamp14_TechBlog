8a09748caa3fe17c6df9d122551799b0b142dccc
Erich Purpur <epurpur@gmail.com>
2021-05-26 10:00:02 -0400

    making api routes for user model

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,19 +1,43 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// this is the /api endpoint
+// this is at the /api endpoint
 
-//GET all users
-router.get('/user', async (req, res) => {
+
+// POST - create a new user
+router.post('/', async (req, res) => {
     try {
-        console.log(`\n Getting all user data \n`);
+        console.log(`\n Making new user: ${req.body.username} \n`);
 
-        const userData = await User.findAll();
-        res.status(200).json(userData);        
-        
+        const userData = await User.create(req.body);
+
+        res.status(200).json(userData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+// DELETE - delete a user
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: { id: req.params.id }
+        });
+
+        // if wrong id entered
+        if (!userData) {
+            res.status(404).json({message: 'no user found with this id'});
+        } else {
+            console.log(`\n Deleting user with id: ${req.params.id} \n`);
+            res.status(200).json(userData);
+        }
+
     } catch (err) {
         res.status(500).json(err);
     }
-});
+})
+
 
 module.exports = router;
