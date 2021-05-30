a9e9fb922c1bb112f453b0657dcda77daa4ee057
Erich Purpur <epurpur@gmail.com>
2021-05-30 12:08:55 -0400

    requirin session module

--- server.js
+++ server.js
@@ -1,41 +1,53 @@
 // DEPENDENCIES
-const express = require('express');
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
-// HANDLEBARS configuration
-// set up Handlebars.js engine
-const hbs = exphbs.create({});
-
-// EXPRESS CONFIGURATION
-// tells node that we are creating an express server
-const app = express();  
-
-// sets an initial port.
+const app = express();
 const PORT = process.env.PORT || 9999;
 
-// inform Express.js on which template engine to use
+
+// Set up Handlebars.js engine
+const hbs = exphbs.create({});
+
+// Create session
+const sess = {
+  secret: 'Super secret session',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Sets up express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Sets location of static public assets (css and client js files)
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-// tells app what routes to use
 app.use(routes);
 
+
 // Logging DB info
-console.log(`Database name: ${sequelize.config.database} \n running on port: ${sequelize.config.port} \n under hostname: ${sequelize.config.host}`)
+console.log(`Database name: ${sequelize.config.database} \n running on port: ${sequelize.config.port} \n under hostname: ${sequelize.config.host}`);
 
 
 // Listener. Ths effectively 'starts' our server
 app.listen(PORT, () => {
     console.log(`Node Server running on port: ${PORT}`);
-})
+});
