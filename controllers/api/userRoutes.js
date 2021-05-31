fcf7344f2e0f6c3a43b4e011dc4b7f1e1114a479
Erich Purpur <epurpur@gmail.com>
2021-05-31 13:27:33 -0400

    refactoring userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,20 +59,6 @@ router.post('/registerUser', async (req, res) => {
 
 
 
-// // user logout route
-// router.post('/logout', (req, res) => {
-//     console.log(`\n  THIS HERE Before: ${req.session.logged_in}  \n`);
-//     if (req.session.logged_in) {
-//         req.session.destroy(() => {
-//             res.status(204).end();
-//         });
-//     } else {
-//         res.status(404).end();
-//     }
-//     console.log(`\n After: ${req.session.logged_in} \n`);
-// });
-
-
 router.post('/logout', (req, res) => {
     console.log(`\n Logged in: ${req.session.logged_in}  \n`);
     
@@ -120,79 +106,6 @@ router.get('/:username', async (req, res) => {
       res.status(500).json(err);
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-// // POST - create a new user
-// router.post('/', async (req, res) => {
-//     try {
-//         console.log(`\n Making new user: ${req.body.username} \n`);
-
-//         const userData = await User.create(req.body);
-
-//         res.status(200).json(userData);
-
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-
-
-// // PUT - modify a user by its 'id' value
-// router.put('/:id', async (req, res) => {
-//     try {
-
-//         const userData = await User.update(
-//             // set all attributes of user to values passed in to req.body
-//             { username: req.body.username,
-//               email: req.body.email,
-//               password: req.body.password },
-//               { individualHooks: true, 
-//                 where: {id: req.params.id} 
-//             }
-//         )
-//         // if wrong id entered
-//         if (!userData) {
-//             res.status(404).json({message: 'no user found with this id'});
-//         } else {
-//             console.log(`\n Editing record id: ${req.body.id} \n`);
-//             res.status(200).json(userData);
-//         }
-
-
-//     } catch (err) {
-//         res.status(400).json(err)
-//     }
-// })
-
-
-// // DELETE - delete a user by its 'id' value
-// router.delete('/:id', async (req, res) => {
-//     try {
-//         const userData = await User.destroy({
-//             where: { id: req.params.id }
-//         });
-
-//         // if wrong id entered
-//         if (!userData) {
-//             res.status(404).json({message: 'no user found with this id'});
-//         } else {
-//             console.log(`\n Deleting user with id: ${req.params.id} \n`);
-//             res.status(200).json(userData);
-//         }
-
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
 
 
 module.exports = router;
